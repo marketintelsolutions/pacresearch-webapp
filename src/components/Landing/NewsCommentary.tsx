139e7b0b8f290e4bdc688a285870a146c1206fc0
igee-17 <igbagboleye@gmail.com>
2025-06-13 09:40:10 +0100

    fix to news commentary

--- src/components/Landing/NewsCommentary.tsx
+++ src/components/Landing/NewsCommentary.tsx
@@ -118,8 +118,8 @@ const NewsCommentary: React.FC = () => {
                 className="h-[452px] w-full md:max-w-[331px] rounded-[30px] cursor-pointer transition-transform hover:scale-105"
                 onClick={() => handleVideoClick(item.videoUrl)}
               >
-                <div className="h-full flex items-end px-5 pb-8 rounded-[30px] bg-gradient-to-b to-[#00000042] from-[#383838fe]">
-                  <div className="flex flex-col gap-[15px]">
+                <div className="h-full flex items-end  rounded-[30px] ">
+                  <div className="flex flex-col rounded-[30px] gap-[15px] px-5 py-8 bg-gradient-to-b to-[#00000042] from-[#383838fe]">
                     <div className="h-1.5 w-[65%] bg-white rounded-full"></div>
                     <p className="justify-start text-white text-xl font-medium font-['Montserrat'] leading-loose tracking-tight [text-shadow:_0px_13px_19px_rgb(0_0_0_/_0.24)]">
                       {item.title} - <span>{item.interviewer}</span>
