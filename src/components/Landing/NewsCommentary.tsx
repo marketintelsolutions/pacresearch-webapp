fa4e7835a39bdc78d1637269ebda9470e6a7b111
marketintelsolutions <info@ziltchone.com>
2025-06-18 19:07:30 +0100

    news background color

--- src/components/Landing/NewsCommentary.tsx
+++ src/components/Landing/NewsCommentary.tsx
@@ -210,7 +210,7 @@ const NewsCommentary: React.FC = () => {
                 onClick={() => handleVideoClick(item.videoUrl)}
               >
                 <div className="h-full flex items-end rounded-[30px]">
-                  <div className="w-full flex flex-col rounded-[30px] gap-[15px] px-5 py-8 bg-gradient-to-b to-[#00000042] from-[#383838fe]">
+                  <div className="w-full flex flex-col rounded-[30px] gap-[15px] px-5 py-8 bg-gradient-to-b to-[#31367042] from-[#062644]">
                     <div className="h-1.5 w-[65%] bg-white rounded-full"></div>
                     <p className="justify-start text-white text-xl font-medium font-['Montserrat'] leading-loose tracking-tight [text-shadow:_0px_13px_19px_rgb(0_0_0_/_0.24)]">
                       {item.title} - <span>{item.interviewer}</span>
